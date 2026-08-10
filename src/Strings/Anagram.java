bf10c7b399b7442af3e258bb08431b3115131fdf
SamuelRobart <samuelolivarobart2000@gmail.com>
2026-08-10 12:15:02 +0530

    Optmise Hashmap method

--- src/Strings/Anagram.java
+++ src/Strings/Anagram.java
@@ -1,28 +1,67 @@
 package Strings;
 
+
+//optimise also -> hashmap
+
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Anagram {
 
     public static void main(String[] args) {
 
 
-        String str1 = "sam";
-        String str2 = "mas";
+        String str1 = "!samam";
+        String str2 = "masam!";
 
-        System.out.println(isAnagram(str1,str2));
+        System.out.println(valid(str1,str2));
     }
 
     public static boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
 
-        int[] freq = new int[26];
 
-        for (int i = 0; i < s.length(); i++) {
+        if(s.length() != t.length() || s.isEmpty())
+            return false;
+
+        int[] freq = new int [26];
+        for(int i=0;i<s.length();i++){
+
             freq[s.charAt(i) - 'a']++;
             freq[t.charAt(i) - 'a']--;
         }
 
-        for (int count : freq) {
-            if (count != 0) return false;
+        for (int j : freq) {
+
+            if (j != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean valid(String s, String t){
+
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+
+
+        for (int i=0;i<s.length();i++){
+
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+
+            map.put(c1,map.getOrDefault(c1,0)+1);
+            map.put(c2,map.getOrDefault(c2,0)-1);
+        }
+
+        for(int count : map.values()){
+            if(count != 0)
+                return false;
         }
 
         return true;
