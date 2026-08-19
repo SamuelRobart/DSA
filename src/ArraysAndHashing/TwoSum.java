c7c275c595f0694140ae1e7dc7c5ec7989e3498d
SamuelRobart <samuelolivarobart2000@gmail.com>
2026-08-19 20:27:43 +0530

    Optimise version

--- src/ArraysAndHashing/TwoSum.java
+++ src/ArraysAndHashing/TwoSum.java
@@ -1,6 +1,8 @@
 package ArraysAndHashing;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 // Unsorted Array
 public class TwoSum {
@@ -11,6 +13,8 @@ public class TwoSum {
         int target = 18;
 
         System.out.println(Arrays.toString(twoSum(arr,target)));
+        System.out.println(Arrays.toString(optTwoSum(arr,target)));
+
     }
 
     public static int[] twoSum(int []arr , int target) {
@@ -27,5 +31,23 @@ public class TwoSum {
         }
 
         return new int [] {};
+    }
+
+    private static int[] optTwoSum(int [] arr, int target){
+
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0;i<arr.length;i++){
+
+            int complement = target - arr[i];
+
+            if(map.containsKey(complement)){
+
+                return new int[]{map.get(complement),i};
+            }
+
+            map.put(arr[i],i);
+        }
+        return  new int []{-1,-1};
     }
 }
